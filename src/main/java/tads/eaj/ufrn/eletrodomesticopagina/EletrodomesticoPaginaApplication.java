8b09c5c1fa063403108f304e334e3a1d1c5ddd85
nadinibzrra <clarinha.nadini@gmail.com>
2021-08-14 01:05:29 -0300

    Initial commit

---
+++ src/main/java/tads/eaj/ufrn/eletrodomesticopagina/EletrodomesticoPaginaApplication.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.eletrodomesticopagina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletrodomesticoPaginaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EletrodomesticoPaginaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tads/eaj/ufrn/eletrodomesticopagina/EletrodomesticoPaginaApplicationTests.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.eletrodomesticopagina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletrodomesticoPaginaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
