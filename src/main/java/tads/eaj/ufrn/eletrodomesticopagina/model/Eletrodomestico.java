18faea2b629879d88f540994c7834d2b38313e9a
nadinibzrra <clarinha.nadini@gmail.com>
2021-08-14 22:09:55 -0300

    validacao

--- src/main/java/tads/eaj/ufrn/eletrodomesticopagina/model/Eletrodomestico.java
+++ src/main/java/tads/eaj/ufrn/eletrodomesticopagina/model/Eletrodomestico.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
@@ -23,17 +22,12 @@ public class Eletrodomestico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Size(min = 3,max = 25, message = "digite um eletrodomestico de nome entre 3 e 25")
-    @NotBlank
     private String nome;
     @Size(min = 3,max = 20)
-    @NotBlank
     private String marca;
     @Size(min = 3,max = 12)
-    @NotBlank
     private String cor;
-    @NotBlank
     private double preco;
-    @NotBlank
     private int potencia;
     private String imageUri;
     private Date deleted;
